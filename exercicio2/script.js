9b14a64e054381bad1ed0ef492ca536e81967989
constance03 <maria-constance@hotmail.com>
2022-09-06 14:34:31 -0300

    exercicios finalizados

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,8 +1,8 @@
 // Escreva um código que receba três valores. Uma idade, um booleano que responda se a pessoa terminou ou não o ensino médio, 
 // e um booleano que responda se a pessoa está cursando alguma faculdade.
 let idade = prompt("Qual sua idade?")
-let isMaiorIdade = confirm("Terminou o ensino médio?")
-let isCursandoOutraFaculdade = confirm("Esta cursando outra faculdade?")
+let isNoEnsinoMedio = confirm("Terminou o ensino médio?")
+let isCursandoFaculdade = confirm("Esta cursando outra faculdade?")
 
 // Crie um if que imprima que a afirmacão é verdadeira, e um else para imprimir que a afirmacão não é verdadeira:
 // Se a pessoa tem 18 anos ou mais;
@@ -13,15 +13,15 @@ if (idade >= 18) {
 }
 
 // Se a pessoa terminou o ensino médio;
-if (isMaiorIdade) {
+if (isNoEnsinoMedio) {
     console.log("Terminou o ensino médio");
 } else {
     console.log("Não terminou o ensino médio");
 }
 
 // Se a pessoa NÃO está cursando alguma faculdade;
-if (!isCursandoOutraFaculdade) {
-    console.log("Não esta cursando outra faculdade");
+if (!isCursandoFaculdade) {
+    console.log("Não está cursando faculdade");
 } else {
-    console.log("Está cursando outra faculdade");
+    console.log("Está cursando faculdade");
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,6 +1,6 @@
 // Crie um código que receba um prompt que diga: "Escreva aqui sua nacionalidade" e guarde o valor da resposta em uma const nacionalidade.
-const nacionalidade = prompt("Insira sua nacionalidade")
-
+let nacionalidade = prompt("Insira sua nacionalidade")
+nacionalidade = nacionalidade.toUpperCase()
 // Vamos considerar que nosso programa aceita as seguintes 5 nacionalidades:
 // brasileira;
 // argentina;
@@ -10,15 +10,15 @@ const nacionalidade = prompt("Insira sua nacionalidade")
 // Crie uma estrutura de if/else if/else que verifique se a nacionalidade guardada na const nacionalidade é igual a 
 // cada uma das nacionalidades acima, e caso seja, imprima a nacionalidade no console. 
 // O último else deve imprimir "nacionalidade não encontrada" caso o valor de nacionalidade não corresponda a nenhum dos valores.
-if (nacionalidade === "BRASILEIRA" || nacionalidade === "brasileira" || nacionalidade === "Brasileira") {
+if (nacionalidade === "BRASILEIRA") {
     console.log(`Sua nacionalidade é: ${nacionalidade}`);
-} else if (nacionalidade === "argentina" || nacionalidade === "Argentina" || nacionalidade === "ARGENTINA") {
+} else if (nacionalidade === "ARGENTINA") {
     console.log(`Sua nacionalidade é: ${nacionalidade}`);
-} else if (nacionalidade === "uruguaia" || nacionalidade === "Uruguaia" || nacionalidade === "URUGUAIA") {
+} else if (nacionalidade === "URUGUAIA") {
     console.log(`Sua nacionalidade é: ${nacionalidade}`);
-} else if (nacionalidade === "chilena" || nacionalidade === "Chilena" || nacionalidade === "CHILENA") {
+} else if (nacionalidade === "CHILENA") {
     console.log(`Sua nacionalidade é: ${nacionalidade}`);
-} else if (nacionalidade === "colombiana" || nacionalidade === "Colombiana" || nacionalidade === "COLOMBIANA") {
+} else if (nacionalidade === "COLOMBIANA") {
     console.log(`Sua nacionalidade é: ${nacionalidade}`);
 } else {
     console.log(`Sua nacionalidade não foi encontrada`);
