2d10de6453c928459953660cdc33154adf33bd99
constance03 <maria-constance@hotmail.com>
2022-09-06 13:50:39 -0300

    exercicios feitos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,27 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+// Escreva um código que receba três valores. Uma idade, um booleano que responda se a pessoa terminou ou não o ensino médio, 
+// e um booleano que responda se a pessoa está cursando alguma faculdade.
+let idade = prompt("Qual sua idade?")
+let isMaiorIdade = confirm("Terminou o ensino médio?")
+let isCursandoOutraFaculdade = confirm("Esta cursando outra faculdade?")
 
-if(){
+// Crie um if que imprima que a afirmacão é verdadeira, e um else para imprimir que a afirmacão não é verdadeira:
+// Se a pessoa tem 18 anos ou mais;
+if (idade >= 18) {
+    console.log("Sim, é maior de idade");
+} else{
+	console.log("É menor de idade");
+}
 
-}else{
-	
+// Se a pessoa terminou o ensino médio;
+if (isMaiorIdade) {
+    console.log("Terminou o ensino médio");
+} else {
+    console.log("Não terminou o ensino médio");
+}
+
+// Se a pessoa NÃO está cursando alguma faculdade;
+if (!isCursandoOutraFaculdade) {
+    console.log("Não esta cursando outra faculdade");
+} else {
+    console.log("Está cursando outra faculdade");
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,25 @@
-let nacionalidade //= adicione o prompt aqui
+// Crie um código que receba um prompt que diga: "Escreva aqui sua nacionalidade" e guarde o valor da resposta em uma const nacionalidade.
+const nacionalidade = prompt("Insira sua nacionalidade")
+
+// Vamos considerar que nosso programa aceita as seguintes 5 nacionalidades:
+// brasileira;
+// argentina;
+// uruguaia;
+// chilena;
+// colombiana;
+// Crie uma estrutura de if/else if/else que verifique se a nacionalidade guardada na const nacionalidade é igual a 
+// cada uma das nacionalidades acima, e caso seja, imprima a nacionalidade no console. 
+// O último else deve imprimir "nacionalidade não encontrada" caso o valor de nacionalidade não corresponda a nenhum dos valores.
+if (nacionalidade === "BRASILEIRA" || nacionalidade === "brasileira" || nacionalidade === "Brasileira") {
+    console.log(`Sua nacionalidade é: ${nacionalidade}`);
+} else if (nacionalidade === "argentina" || nacionalidade === "Argentina" || nacionalidade === "ARGENTINA") {
+    console.log(`Sua nacionalidade é: ${nacionalidade}`);
+} else if (nacionalidade === "uruguaia" || nacionalidade === "Uruguaia" || nacionalidade === "URUGUAIA") {
+    console.log(`Sua nacionalidade é: ${nacionalidade}`);
+} else if (nacionalidade === "chilena" || nacionalidade === "Chilena" || nacionalidade === "CHILENA") {
+    console.log(`Sua nacionalidade é: ${nacionalidade}`);
+} else if (nacionalidade === "colombiana" || nacionalidade === "Colombiana" || nacionalidade === "COLOMBIANA") {
+    console.log(`Sua nacionalidade é: ${nacionalidade}`);
+} else {
+    console.log(`Sua nacionalidade não foi encontrada`);
+}
